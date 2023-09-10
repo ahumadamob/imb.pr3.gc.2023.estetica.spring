f2cff7dcc1b1de445ea93585ac42c3faab57e98b
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2023-09-10 20:03:59 -0300

    se corrige linea de nombre del paquete

--- src/main/java/imb/pr3/estetica/repositorio/ServicioRepositorio.java
+++ src/main/java/imb/pr3/estetica/repositorio/ServicioRepositorio.java
@@ -1,6 +1,5 @@
 package imb.pr3.estetica.repositorio;
 
-import imb.pr3.estetica.entidad.MetodoDePago;
 import imb.pr3.estetica.entidad.Servicio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
