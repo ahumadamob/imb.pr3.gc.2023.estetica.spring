b4539c872f514efeefeb613cc19f5a79aa8dd801
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2023-09-19 12:00:03 -0300

    Se modifica para que devuelva un Objeto

--- src/main/java/imb/pr3/estetica/service/IServicioService.java
+++ src/main/java/imb/pr3/estetica/service/IServicioService.java
@@ -4,11 +4,13 @@ import imb.pr3.estetica.entity.Servicio;
 
 import java.util.List;
 
-public interface IServicioService<I> {
-    List<Servicio> buscarServicio();
-    Servicio buscarServicioPorId(Integer id);
-    public void guardarServicio(Servicio cliente);
-    public void eliminarServicio (Integer id);
-    public void crearServicio (Servicio cliente);
+public interface IServicioService {
+    public List<Servicio> buscarServicio();
+
+    public Servicio buscarServicioPorId(Integer id);
+
+    public Servicio guardarServicio(Servicio cliente);
+
+    public Servicio eliminarServicio(Integer id);
 
 }
