04f55d709256deb55c868b0439d876d2cc73582f
dev-NIK0 <nickitow6@gmail.com>
2023-10-06 17:32:43 -0300

    cambios listos

--- src/main/java/imb/pr3/estetica/repository/MetodoDePagoRepository.java
+++
@@ -1,9 +0,0 @@
-package imb.pr3.estetica.repository;
-
-import imb.pr3.estetica.entity.MetodoDePago;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MetodoDePagoRepository extends JpaRepository<MetodoDePago, Integer> {
-
-}
-
---
+++ src/main/java/imb/pr3/estetica/service/jpa/IEmpleadoService.java
@@ -0,0 +1,16 @@
+package imb.pr3.estetica.service.jpa;
+
+import java.util.List;
+
+import imb.pr3.estetica.entity.Empleado;
+
+
+public interface IEmpleadoService {
+	List<Empleado> buscarTodos() ;
+	public Empleado buscarPorId(int id) ;
+	/*public void nuevoObjeto(Empleado empleado);*/
+	public void eliminar(int id);
+	public Empleado guardar(Empleado empleado) ;
+	public boolean existe(Integer id);
+
+}
